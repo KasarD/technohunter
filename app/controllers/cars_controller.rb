6518d6852f2b12b06d145e67475444b4b4421819
Andrew Gaev <kasard08@gmail.com>
2016-10-24 05:59:52 +0300

    [RB] Редактирование CarsController Почти закончили с логикой работы контроллера, наследуемого от Vehicles

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,7 +1,22 @@
 class CarsController < VehiclesController
 
+    # Расширяем функционал специально для типа легкового транспорта
     def all_vehicles
         @cars = Car.where(moderate: true).order("created_at DESC")
+    end
+    # Создание нового объекта легкового транспорта
+    # Переопределен от родительского класса
+    def new_vehicle (params)
+        @car = Car.new(params)
+    end
+
+private
+    # Strong Params. Перечисляем необходимые для создания параметры
+    def vehicle_params
+        params.require(:car).permit(
+            :vehicle_type, :body, :description, :engine, :engine_cap, :kpp,
+            :vehicle_status, :price, :drive, :mileage, :vin_code, :year
+        )
     end
 
 end
--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,4 +1,5 @@
 class VehiclesController < ApplicationController
+    before_action :set_vehicle, only: [:show, :edit, :update, :destroy]
 
     def all_vehicles
         # Заглушка для наследования контроллеров
@@ -6,7 +7,55 @@ class VehiclesController < ApplicationController
     def index
         # Получаем все объекты вызванной нами модели
         @vehicles = all_vehicles
-        # instance_variable_set("@#{controller_name}", all_vehicles)
+    end
+
+    def show
+    end
+
+    def new
+        # Создаем новый экземпляр класса без параметров
+        @vehicle = new_vehicle (nil)
+    end
+
+    def edit
+    end
+
+    def create
+        # Создаем новый экземпляр класса с параметрами, взятыми из формы
+        @vehicle = new_vehicle(vehicle_params)
+        respond_to do |format|
+            # Если сохранение успешно, то переходим в созданное объявление
+            if @vehicle.save
+                format.html { redirect_to @vehicle, notice: 'Successfully create this offer!'}
+            else
+            # Если же нет, то рендерим заново вьюху и передаем ошибки
+                format.html { render :new }
+            end
+        end
+    end
+
+    def update
+        respond_to do |format|
+            # Аналогично методу create
+            if @vehicle.update(vehicle_params)
+                format.html { redirect_to @vehicle, notice: "Vehicle successfully updated." }
+            else
+                format.html { redirect_to :edit }
+            end
+        end
+    end
+
+    def destroy
+        @vehicle.destroy
+        # Переходим на главную страницу
+        redirect_to :home
+    end
+
+private
+    # Вызываем колбэком
+    def set_vehicle
+        # Т.к. id у всех разные, данный метод является универсальным
+        @vehicle = Vehicle.find(params[:id])
     end
     
 end

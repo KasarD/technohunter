82521156526494c8df25b5e084215e429f5c5f53
Andrew Gaev <kasard08@gmail.com>
2016-10-24 19:11:14 +0300

    [RB,HTML] Созданы контроллеры Equips и Trucks Данные контроллеры созданы по подобию Cars, то есть наследованы от Vehicles с переоперделением методов

--- app/controllers/equips_controller.rb
+++ app/controllers/equips_controller.rb
@@ -1,7 +1,16 @@
 class EquipsController < VehiclesController
 
-    def all_vehicles
+    def index
         @equips = Equip.where(moderate: true).order("created_at DESC")
     end
+    def new_vehicle
+        @equip = Equip.new
+    end
 
+private
+    def vehicle_params
+        params.require(:equip).permit(
+            :vehicle_type, :description, :status, :price, :vin_code, :year, :h_of_use
+        )
+    end
 end
--- app/controllers/trucks_controller.rb
+++ app/controllers/trucks_controller.rb
@@ -1,7 +1,18 @@
 class TrucksController < VehiclesController
 
-    def all_vehicles
-        @trucks = Truck.where(moderate: true).order("created_at DESC")
+    def index
+        @trucks = Truck.where(moderate: true)
     end
 
+    def new_vehicle (params)
+        @truck = Truck.new(params)
+    end
+
+private
+    def vehicle_params
+        params.require(:truck).permit(
+            :vehicle_type, :vin_code, :year, :engine, :engine_cap,
+            :h_of_use, :status, :description, :price, :mileage
+        )
+    end
 end

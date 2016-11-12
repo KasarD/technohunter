ba5a491bc5dd5f817f4530cacbe0104f3ff7e11a
Andrew Gaev <kasard08@gmail.com>
2016-11-12 11:03:31 +0300

    [RB] Изменен стиль кода для тестов Стиль изменен в соответствии с SyleGuide для Ruby

--- spec/controllers/cars_controller_spec.rb
+++ spec/controllers/cars_controller_spec.rb
@@ -1,139 +1,142 @@
 require 'rails_helper'
 
 describe CarsController do
+  render_views
 
-    render_views
-
-    describe "GET #index" do
-        it "assigns an array of cars to @cars" do
-            car = create(:car)
-            get :index
-            expect(assigns(:cars)).to eq([car])
-        end
-
-        it "renders the car :index view" do
-            get :index
-            expect(response).to render_template 'cars/index'
-        end
+  describe 'GET #index' do
+    it 'assigns an array of cars to @cars' do
+      car = create(:car)
+      get :index
+      expect(assigns(:cars)).to eq([car])
     end
 
-    describe "GET #show" do 
-        before (:all) do
-            @car = create(:car)
-        end
-        it "assigns requested car to @vehicle" do
-            get :show, id: @car
-            expect(assigns(:vehicle)).to eq(@car)
-        end    
-        it "render the car :show view" do
-            get :show, id: @car
-            expect(response).to render_template 'cars/show'
-        end
-        it "render the shared/form partial" do
-            get :show , id: @car
-            expect(response).to render_template :partial => 'shared/_show_vehicle'
-        end
-        after(:all) do
-            @car.destroy
-        end
+    it 'renders the car :index view' do
+      get :index
+      expect(response).to render_template 'cars/index'
+    end
+  end
+
+  describe 'GET #show' do
+    before (:all) do
+      @car = create(:car)
     end
 
-    describe "GET #new" do
-
-        it "create new instance of Car with Vehicle parent" do
-            expect(described_class).to be < VehiclesController    
-        end
-        it "assigns new car instance to @vahicle" do
-            get :new
-            expect(assigns(:car).type).to eq("Car")
-        end
-        it "render the car :_form template" do
-            get :new
-            expect(response).to render_template 'cars/_form'
-        end
-
+    it 'assigns requested car to @vehicle' do
+      get :show, id: @car
+      expect(assigns(:vehicle)).to eq(@car)
     end
 
-    describe "GET #edit" do
-        before (:all) do
-            @car = create(:car)
-        end
-        it "assigns requested car to @vehicle" do
-            get :edit, id: @car
-            expect(assigns(:vehicle)).to eq(@car)
-        end
-        it "render the car :form template" do
-            get :edit, id: @car
-            expect(response).to render_template 'cars/_form'
-        end
-        after (:all) do
-            @car.destroy
-        end
+    it 'render the car :show view' do
+      get :show, id: @car
+      expect(response).to render_template 'vehicles/show'
     end
 
-    describe "POST #create" do
-        context "with valid attributes" do
-            it "saves the new car in the database" do
-                expect{
-                    post :create, car: attributes_for(:car)
-                }.to change(Car,:count).by(1)
-            end
-            it "redirects to the new car :show view" do
-                post :create, car: attributes_for(:car)
-                expect(response).to redirect_to Car.last
-            end
-        end
+    it 'render the cars/_show_additional partial' do
+      get :show, id: @car
+      expect(response).to render_template partial: 'cars/_show_additional'
+    end
+    after(:all) do
+      @car.destroy
+    end
+  end
 
-#        context "with invalid attributes" do
-#            it "doesn't save new car" do
-#                expect{
-#                    post :create, car: attributes_for(:invalid_car)
-#                }.to_not change(Car, :count)
-#            end
-#            it "renders the vehicle :new template"
-#        end
+  describe 'GET #new' do
+    it 'create new instance of Car with Vehicle parent' do
+      expect(described_class).to be < VehiclesController
+    end
+    it 'assigns new car instance to @vahicle' do
+      get :new
+      expect(assigns(:car).type).to eq('Car')
+    end
+    it 'render the car :_form template' do
+      get :new
+      expect(response).to render_template 'cars/_form'
+    end
+  end
+
+  describe 'GET #edit' do
+    before (:all) do
+      @car = create(:car)
+    end
+    it 'assigns requested car to @vehicle' do
+      get :edit, id: @car
+      expect(assigns(:vehicle)).to eq(@car)
+    end
+    it 'render the car :form template' do
+      get :edit, id: @car
+      expect(response).to render_template 'cars/_form'
+    end
+    after (:all) do
+      @car.destroy
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves the new car in the database' do
+        expect do
+          post :create, car: attributes_for(:car)
+        end.to change(Car, :count).by(1)
+      end
+      it 'redirects to the new car :show view' do
+        post :create, car: attributes_for(:car)
+        expect(response).to redirect_to Car.last
+      end
     end
 
-    describe "PUT #update" do
-        # TODO: Доделать данный метод (everydayrails.com)
-        before(:all) do
-            @car = create(:car)
-        end
-        context "with valid attributes" do
-            it "locates the requested car" do
-                get :update, id: @car, car: attributes_for(:car)
-                expect(assigns(:vehicle)).to eq(@car)    
-            end
+    context 'with invalid attributes' do
+      it "doesn't save new car in db" do
+        expect do
+          post :create, car: attributes_for(:car)
+        end.to change(Car, :count).by(0)
+      end
 
-            it "change the @car params" do
-                get :update, id: @car, car: attributes_for(:car, vehicle_type: "Фургон")
-                @car.reload
-                expect(@car.vehicle_type).to eq("Фургон")
-            end
+      it 'render :new template' do
+        post :create, car: attributes_for(:car)
+        expect(response).to render_template 'cars/_form'
+      end
+    end
+  end
 
-            it "redirects to the updated @car" do
-                get :update, id: @car, car: attributes_for(:car)
-                expect(response).to redirect_to @car
-            end
-        end
-        after(:all) do
-            @car.destroy
-        end
+  describe 'PUT #update' do
+    before(:all) do
+      @car = create(:car)
+    end
+    context 'with valid attributes' do
+      it 'locates the requested car' do
+        get :update, id: @car, car: attributes_for(:car)
+        expect(assigns(:vehicle)).to eq(@car)
+      end
+
+      it 'change the @car params' do
+        get :update, id: @car, car: attributes_for(:car, vehicle_type: 'Фургон')
+        @car.reload
+        expect(@car.vehicle_type).to eq('Фургон')
+      end
+
+      it 'redirects to the updated @car' do
+        get :update, id: @car, car: attributes_for(:car)
+        expect(response).to redirect_to @car
+      end
+    end
+    after(:all) do
+      @car.destroy
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    before (:each) do
+      @car = create(:car)
     end
 
-    describe "DELETE #destroy" do
-        before (:each) do
-            @car = create(:car)
-        end
-
-        it "deletes car from database" do
-            expect{
-                delete :destroy, id: @car
-            }.to change(Car,:count).by(-1)
-        end
-        it "redirect to the car :index template" do
-            delete :destroy, id: @car
-            expect(response).to redirect_to cars_path
-        end
+    it 'deletes car from database' do
+      expect do
+        delete :destroy, id: @car
+      end.to change(Car, :count).by(-1)
     end
+    it 'redirect to the car :index template' do
+      delete :destroy, id: @car
+      expect(response).to redirect_to cars_path
+    end
+  end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,77 +1,87 @@
 FactoryGirl.define do
-    factory :region do |f|
-        f.name "Московский"
-    end
+  factory :region do |f|
+    f.name 'Бурятия'
+  end
 
-    factory :city do |f|
-        f.name "Москва"
-        f.association :region, factory: :region 
-    end
-    
-    factory :vehicle do |f|
-        f.vehicle_type "test"
-        f.year         "1997"
-        f.price        29000
-        f.city_id      1
-        f.region_id    2
-    end
+  factory :city do |f|
+    f.name 'Бичура'
+    f.association :region, factory: :region
+  end
 
-    factory :car do |car|
-        car.vehicle_type "Седан"
-        car.description  "Тестовое описание"
-        car.engine       "Бензин"
-        car.engine_cap   "1.6"
-        car.kpp          "Механика"
-        car.moderate     true
-        car.price        780000
-        car.drive        "Передний"
-        car.mileage      500
-        car.vin_code     "1234554321"
-        car.year         2007
-        car.association  :region, factory: :region
-        car.association  :city, factory: :city      
-    end
+  factory :brand do |f|
+    f.name 'Aro'
+  end
+  factory :model do |f|
+    f.name '10'
+    f.association :brand, factory: :brand
+  end
 
-    factory :invalid_car, parent: :car do |car|
-        car.vehicle_type nil
-    end
+  factory :vehicle do |f|
+    f.vehicle_type 'test'
+    f.year         '1997'
+    f.price        29_000
+    f.city_id      1
+    f.region_id    2
+  end
 
-    factory :vehicle_image do |f|
-        f.photo { File.new("#{Rails.root}/spec/support/1.jpg") }
-    end
+  factory :car do |car|
+    car.vehicle_type 'Седан'
+    car.description  'Тестовое описание'
+    car.engine       'Бензин'
+    car.engine_cap   '1.6'
+    car.kpp          'Механика'
+    car.moderate     true
+    car.price        780_000
+    car.drive        'Передний'
+    car.mileage      500
+    car.vin_code     '1234554321'
+    car.year         2007
+    car.association  :region, factory: :region
+    car.association  :city, factory: :city
+    car.association  :brand, factory: :brand
+    car.association  :model, factory: :model
+  end
 
-    factory :truck do |truck|
-        truck.vehicle_type "Тягач"
-        truck.description "Тестовый тягач"
-        truck.engine "Дизель"
-        truck.engine_cap "6"
-        truck.moderate true
-        truck.status "1"
-        truck.mileage 100000
-        truck.year 2002
-        truck.vin_code 11111111111
-        truck.h_of_use 700
-        truck.price 2000000
-        truck.association :region, factory: :region
-        truck.association :city, factory: :city
-    end
+  factory :invalid_car, parent: :car do |car|
+    car.vehicle_type nil
+  end
 
-    factory :invalid_truck, parent: :truck do |truck|
-        truck.vehicle_type nil
-    end
+  factory :vehicle_image do |f|
+    f.photo { File.new("#{Rails.root}/spec/support/1.jpg") }
+  end
 
-    factory :equip do |equip|
-        equip.vehicle_type "Токарный станок"
-        equip.description "Тестовый станок"
-        equip.moderate true
-        equip.status "1"
-        equip.year 2005
-        equip.vin_code 11111111111
-        equip.h_of_use 7000
-        equip.price 1200000
-    end
+  factory :truck do |truck|
+    truck.vehicle_type 'Тягач'
+    truck.description 'Тестовый тягач'
+    truck.engine 'Дизель'
+    truck.engine_cap '6'
+    truck.moderate true
+    truck.status '1'
+    truck.mileage 100_000
+    truck.year 2002
+    truck.vin_code 11_111_111_111
+    truck.h_of_use 700
+    truck.price 2_000_000
+    truck.association :region, factory: :region
+    truck.association :city, factory: :city
+  end
 
-    factory :invalid_equip, parent: :equip do |equip|
-        equip.vehicle_type nil
-    end
+  factory :invalid_truck, parent: :truck do |truck|
+    truck.vehicle_type nil
+  end
+
+  factory :equip do |equip|
+    equip.vehicle_type 'Токарный станок'
+    equip.description 'Тестовый станок'
+    equip.moderate true
+    equip.status '1'
+    equip.year 2005
+    equip.vin_code 11_111_111_111
+    equip.h_of_use 7000
+    equip.price 1_200_000
+  end
+
+  factory :invalid_equip, parent: :equip do |equip|
+    equip.vehicle_type nil
+  end
 end
